0cfc5699cd282ce25bc733d613c0e7a73f041e1d
iampradeep1282@gmail.com <iampradeep1282@gmail.com>
2023-12-17 20:23:48 +0530

    create passworde generator

---
+++ App.js
@@ -0,0 +1,21 @@
+function generatePassword() {
+     var length = document.getElementById('length').value;
+     var uppercase = document.getElementById('uppercase').checked;
+     var lowercase = document.getElementById('lowercase').checked;
+     var numbers = document.getElementById('numbers').checked;
+     var symbols = document.getElementById('symbols').checked;
+     
+     var charset = '';
+     if (uppercase) charset += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+     if (lowercase) charset += 'abcdefghijklmnopqrstuvwxyz';
+     if (numbers) charset += '0123456789';
+     if (symbols) charset += '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+     
+     var password = '';
+     for (var i = 0; i < length; i++) {
+       var randomIndex = Math.floor(Math.random() * charset.length);
+       password += charset[randomIndex];
+     }
+     
+     document.getElementById('password').value = password;
+   }
